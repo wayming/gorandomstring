858ab8dc64630f614043dd3c7202142ba1606fd3
wayming <wayming.z@gmail.com>
2020-12-17 01:12:04 +1000

    go random string

---
+++ gotest/first.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+const charset = "abcdefghijklmnopqrstuvwxyz" +
+	"ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+
+func randomString(length int, charset string) string {
+	var seed *rand.Rand = rand.New(rand.NewSource(time.Now().UnixNano()))
+
+	b := make([]byte, length)
+	for i := range b {
+		b[i] = charset[seed.Intn(len(charset))]
+	}
+	return string(b)
+}
+func main() {
+	threads := 1000
+	iterations := 1000
+	completeChan := make(chan int)
+	for i := 0; i < threads; i++ {
+		go func() {
+			for k := 0; k < iterations; k++ {
+				fmt.Println(randomString(k, charset))
+			}
+			completeChan <- 1
+		}()
+	}
+
+	for i := 0; i < threads; i++ {
+		<-completeChan
+	}
+}

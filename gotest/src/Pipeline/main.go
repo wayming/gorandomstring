8671b1a263836a8dad93924f4d5a362bfceeb051
wayming <wayming.z@gmail.com>
2020-12-25 19:57:04 +1000

    channel

--- gotest/src/Pipeline/main.go
+++ gotest/src/Pipeline/main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"path/filepath"
 	"strconv"
 	"strings"
 	"time"
@@ -19,32 +20,44 @@ type StockData struct {
 }
 
 func main() {
-	file, err := os.Open("./F_C.txt")
+
+	// files, err := filepath.Glob("../../../tickerData/F_C.txt")
+	files, err := filepath.Glob("../../../tickerData/*txt")
 	if err != nil {
 		panic(err)
 	}
-	defer file.Close()
-	priceDataChan := make(chan StockData)
-	go func() {
-		reader := bufio.NewReader(file)
-		for {
-			line, _, err := reader.ReadLine()
-			if err == io.EOF {
-				break
-			}
-			tokens := strings.Split(string(line), ",")
-			priceDate, _ := time.Parse("20060102", tokens[0])
-			priceOpen, _ := strconv.ParseFloat(tokens[1], 64)
-			priceHigh, _ := strconv.ParseFloat(tokens[2], 64)
-			priceLow, _ := strconv.ParseFloat(tokens[3], 64)
-			priceClose, _ := strconv.ParseFloat(tokens[4], 64)
-			data := StockData{priceDate, priceOpen, priceHigh, priceLow, priceClose}
-			priceDataChan <- data
+	priceDataChan := make(chan StockData, 10240)
+	done := make(chan int)
+	fmt.Println(files)
+	// Stage1 - Process files in parallel
+	for _, fileName := range files {
+		file, err := os.Open(fileName)
+		if err != nil {
+			panic(err)
 		}
-		close(priceDataChan)
-	}()
+		defer file.Close()
+		go func() {
+			reader := bufio.NewReader(file)
+			for {
+				line, _, err := reader.ReadLine()
+				if err == io.EOF {
+					break
+				}
+				tokens := strings.Split(string(line), ",")
+				priceDate, _ := time.Parse("20060102", tokens[0])
+				priceOpen, _ := strconv.ParseFloat(tokens[1], 64)
+				priceHigh, _ := strconv.ParseFloat(tokens[2], 64)
+				priceLow, _ := strconv.ParseFloat(tokens[3], 64)
+				priceClose, _ := strconv.ParseFloat(tokens[4], 64)
+				data := StockData{priceDate, priceOpen, priceHigh, priceLow, priceClose}
+				priceDataChan <- data
+			}
+			done <- 1
+		}()
+	}
 
-	upChan := make(chan StockData)
+	// Stage 2 - filter the stock that goes up
+	upChan := make(chan StockData, 10240)
 	go func() {
 		for price := range priceDataChan {
 			if price.close > price.open {
@@ -54,11 +67,35 @@ func main() {
 		close(upChan)
 	}()
 
-	for price := range upChan {
-		fmt.Printf("%s, open %f, close %f, diff %f\n",
-			price.date.String(),
-			price.open,
-			price.close,
-			price.close-price.open)
+	// Stage 3 - display greatest diff
+	maxChan := make(chan StockData)
+	go func() {
+		var max StockData
+		for new := range upChan {
+			fmt.Println(max.close - max.open)
+			fmt.Println(new.close - new.open)
+
+			if max.close-max.open < new.close-new.open {
+				max = new
+			}
+
+			fmt.Println(max)
+		}
+		maxChan <- max
+	}()
+
+	// Wait until stage 1 is done
+	for range files {
+		<-done
 	}
+	close(priceDataChan)
+	close(done)
+
+	max := <-maxChan
+	fmt.Printf("%s, open %f, close %f, diff %f\n",
+		max.date.String(),
+		max.open,
+		max.close,
+		max.close-max.open)
+
 }
